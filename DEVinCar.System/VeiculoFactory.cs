1557e901a195ddbc695c317d25f1894ecdf2e9c1
Reibnitz <lucas.fpolis@gmail.com>
2022-07-11 14:28:06 -0300

    add: VeiculoFactory

---
+++ DEVinCar.System/VeiculoFactory.cs
@@ -0,0 +1,28 @@
+﻿using DEVinCar.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVinCar.System
+{
+    public class VeiculoFactory
+    {
+        private ControleDeProducao _controleDeProducao;
+
+        public VeiculoFactory(ControleDeProducao controleDeProducao)
+        {
+            _controleDeProducao = controleDeProducao;
+        }
+
+        public IVeiculo CriarCarro(int potencia, int numeroDePortas, bool ehFlex, string nome, string placa, double valor, string cor, DateTime dataDeFabricacao)
+        {
+            // TODO: adicionar validações
+            Carro carro = new(potencia, numeroDePortas, ehFlex, nome, placa, valor, cor, dataDeFabricacao);
+            _controleDeProducao.AdicionarVeiculo(carro);
+
+            return carro;
+        }
+    }
+}
