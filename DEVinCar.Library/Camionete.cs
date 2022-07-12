91c9d37e2cd1b58f6ddcf60c88f04e68fd93c49f
Marina Hoffmann <marinasthoffmann@gmail.com>
2022-07-12 11:42:58 -0300

    change: constructor atualizado

--- DEVinCar.Library/Camionete.cs
+++ DEVinCar.Library/Camionete.cs
@@ -8,14 +8,19 @@ namespace DEVinCar.Library
 {
     public class Camionete : Veiculo
     {
-        public Camionete(string nome, string placa, double valor, string cor, DateTime dataDeFabricacao) : base(nome, placa, valor, cor, dataDeFabricacao)
-        {
-        }
 
         public int NumeroDePortas { get; set; }
         public int CapacidadeDaCacamba { get; set; }
         public int Potencia { get; set; }
         public string TipoCombustivel { get; set; } // ??
+        public Camionete(int numeroDePortas, int capacidadeDaCacamba, int potencia, string tipoCombustivel,
+            string nome, string placa, double valor, string cor, DateTime dataDeFabricacao) : base(nome, placa, valor, cor, dataDeFabricacao)
+        {
+            NumeroDePortas = numeroDePortas;
+            CapacidadeDaCacamba = capacidadeDaCacamba;
+            Potencia = potencia;
+            TipoCombustivel = tipoCombustivel;
+        }
 
         public override string ListarInformacoes()
         {
