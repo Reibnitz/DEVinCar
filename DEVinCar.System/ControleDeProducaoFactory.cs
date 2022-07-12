fc8da0332c23c47a89697974573c8c0d29cc486f
Marina Hoffmann <marinasthoffmann@gmail.com>
2022-07-12 11:47:04 -0300

    create: ControleDeProducaoFactory

---
+++ DEVinCar.System/ControleDeProducaoFactory.cs
@@ -0,0 +1,16 @@
+﻿using DEVinCar.Library;
+
+namespace DEVinCar.System
+{
+    public class ControleDeProducaoFactory
+    {
+        public static ControleDeProducao CriarControleDeProducao()
+        {
+            List<IVeiculo> listaDeVeiculos = new();
+            ChassiService chassiService = new();
+            ListagemService listagemService  = new();
+
+             return new ControleDeProducao(listaDeVeiculos, chassiService, listagemService);
+        }
+    }
+}
