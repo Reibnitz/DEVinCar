cd0f8923405d080fa24ee06fe957e3c0e78da08a
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-07-12 16:59:54 -0300

    add: exceptions de input

---
+++ DEVinCar.Library/Exceptions/InputInvalidoException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVinCar.Library
+{
+    public class InputInvalidoException : Exception
+    {
+        private const string Mensagem = "ERRO - Input inválido";
+
+        public InputInvalidoException() : base(Mensagem)
+        {
+
+        }
+    }
+}
---
+++ DEVinCar.Library/Exceptions/OpcaoNaoExistenteException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVinCar.Library
+{
+    public class OpcaoNaoExistenteException : Exception
+    {
+        private const string Mensagem = "ERRO - Opção {0} não é válida";
+
+        public OpcaoNaoExistenteException(int opcaoEscolhida) : base(string.Format(Mensagem, opcaoEscolhida))
+        {
+
+        }
+    }
+}
