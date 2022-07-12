91fd7341efcc3b8bcb3b26fc67e1eee16dc0c760
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-07-12 17:00:37 -0300

    add: interface inicial para usuário

--- DEVinCar.System/Program.cs
+++ DEVinCar.System/Program.cs
@@ -13,3 +13,48 @@ foreach (var item in lista)
 {
     Console.WriteLine(item.ListarInformacoes());
 }
+
+
+
+while (true)
+{
+    EOpcaoInicial opcaoInicial = MensagensConsole.EscolherOpcaoInicial();
+    if (opcaoInicial == EOpcaoInicial.AdicionarVeiculo)
+    {
+        EOpcaoTipoVeiculo opcaoVeiculo = MensagensConsole.EscolherOpcaoDeVeiculoParaAdicionar();
+        switch (opcaoVeiculo)
+        {
+            case EOpcaoTipoVeiculo.MotoTricilo:
+                // TODO criar mensagens para adição de veículos
+                break;
+            case EOpcaoTipoVeiculo.Carro:
+                break;
+            case EOpcaoTipoVeiculo.Camionete:
+                break;
+        }
+    }
+    if (opcaoInicial == EOpcaoInicial.ListarVeiculos)
+    {
+        EOpcaoListagem opcaoListagem = MensagensConsole.EscolherOpcaoDeListagem();
+        switch (opcaoListagem)
+        {
+            // TODO adicionar método ToString às listagens
+
+            case EOpcaoListagem.Todos:
+                controleDeProducao.BuscarListaGeral();
+                break;
+
+            case EOpcaoListagem.Disponiveis:
+                controleDeProducao.BuscarListaDeCarrosDisponiveis();
+                break;
+
+            case EOpcaoListagem.Vendidos:
+                EOpcaoOrdenacao opcaoOrdenacao = MensagensConsole.EscolherOpcaoDeOrdenacao();
+                if (opcaoOrdenacao == EOpcaoOrdenacao.Crescente)
+                    controleDeProducao.BuscarListaVendidosPorMenorPreco();
+                if (opcaoOrdenacao == EOpcaoOrdenacao.Decrescente)
+                    controleDeProducao.BuscarListaVendidosPorMaiorPreco();
+                break;
+        }
+    }
+}
