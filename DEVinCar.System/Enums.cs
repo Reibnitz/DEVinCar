5406f6cd97fbaec10b20e969dbdf9f750d2f5714
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-07-12 17:00:10 -0300

    add: enum

---
+++ DEVinCar.System/Enums.cs
@@ -0,0 +1,29 @@
+﻿namespace DEVinCar.System
+{
+    public enum EOpcaoInicial
+    {
+        AdicionarVeiculo = 1,
+        ListarVeiculos
+    }
+
+    public enum EOpcaoTipoVeiculo
+    {
+        MotoTricilo = 1,
+        Carro,
+        Camionete,
+        Todos
+    }
+
+    public enum EOpcaoListagem
+    {
+        Todos = 1,
+        Disponiveis,
+        Vendidos
+    }
+
+    public enum EOpcaoOrdenacao
+    {
+        Crescente = 1,
+        Decrescente
+    }
+}
--- DEVinCar.System/MensagensConsole.cs
+++ DEVinCar.System/MensagensConsole.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DEVinCar.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,39 +9,75 @@ namespace DEVinCar.System
 {
     public class MensagensConsole
     {
-        public static void OpcoesIniciais()
+        public static EOpcaoInicial EscolherOpcaoInicial()
         {
             Console.WriteLine("---------- Bem-vindo ao sistema DEVinCar ----------");
             Console.WriteLine("1- Adicionar veículo");
             Console.WriteLine("2- Buscar veículos no sistema");
+            Console.Write("\nEscolha a opção desejada: ");
+
+            int opcaoEscolhida = VerificarInput(limite: 2);
+
+            return (EOpcaoInicial)opcaoEscolhida;
         }
 
-        public static void OpcoesDeVeiculosParaAdicionar()
+        public static EOpcaoTipoVeiculo EscolherOpcaoDeVeiculoParaAdicionar()
         {
             Console.WriteLine("\n1- Moto/Triciclo");
             Console.WriteLine("2- Carro");  
             Console.WriteLine("3- Camionete");
+            Console.Write("\nEscolha a opção desejada: ");
+
+            int opcaoEscolhida = VerificarInput(limite: 3);
+
+            return (EOpcaoTipoVeiculo)opcaoEscolhida;
         }
 
-        public static void OpcoesGeraisDeListagem()
+        public static EOpcaoListagem EscolherOpcaoDeListagem()
         {
             Console.WriteLine("\n1- Listar todos os veículos");
             Console.WriteLine("2- Listar veículos disponíveis");
-            Console.WriteLine("1- Listar veículos vendidos");
+            Console.WriteLine("3- Listar veículos vendidos");
+            Console.Write("\nEscolha a opção desejada: ");
+
+            int opcaoEscolhida = VerificarInput(limite: 3);
+
+            return (EOpcaoListagem)opcaoEscolhida;
         }
 
-        public static void OpcoesDeVeiculosListagem()
+        public static EOpcaoTipoVeiculo EscolherVeiculoParaListagem()
         {
             Console.WriteLine("\n1- Motos/Triciclos");
             Console.WriteLine("2- Carros");
             Console.WriteLine("3- Camionetes");
             Console.WriteLine("4- Todos");
+            Console.Write("\nEscolha a opção desejada: ");
+
+            int opcaoEscolhida = VerificarInput(limite: 4);
+
+            return (EOpcaoTipoVeiculo)opcaoEscolhida;
         }
 
-        public static void OpcoesDeOrdemDePreco()
+        public static EOpcaoOrdenacao EscolherOpcaoDeOrdenacao()
         {
             Console.WriteLine("\n1- Listar por menor preço vendido");
             Console.WriteLine("2- Listar por maior preço vendido");
+            Console.Write("\nEscolha a opção desejada: ");
+
+            int opcaoEscolhida = VerificarInput(limite: 2);
+
+            return (EOpcaoOrdenacao)opcaoEscolhida;
+        }
+
+        private static int VerificarInput(int limite)
+        {
+            bool respostaValida = int.TryParse(Console.ReadLine(), out int opcaoEscolhida);
+            if (!respostaValida)
+                throw new InputInvalidoException();
+            if (opcaoEscolhida > limite)
+                throw new OpcaoNaoExistenteException(opcaoEscolhida);
+
+            return opcaoEscolhida;
         }
     }
 }
