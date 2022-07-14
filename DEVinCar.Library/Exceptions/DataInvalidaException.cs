a66fc1e01c5ed84b0cdabbc4fe0ae7348a1a38c5
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-07-14 10:18:00 -0300

    create: exception

---
+++ DEVinCar.Library/Exceptions/DataInvalidaException.cs
@@ -0,0 +1,12 @@
+﻿namespace DEVinCar.Library
+{
+    public class DataInvalidaException : Exception
+    {
+        private const string Mensagem = "ERRO - A data de fabricação não pode ser maior que a data atual";
+
+        public DataInvalidaException() : base(Mensagem)
+        {
+
+        }
+    }
+}
---
+++ DEVinCar.Library/Exceptions/ValorNaoPositivoException.cs
@@ -0,0 +1,12 @@
+﻿namespace DEVinCar.Library
+{
+    public class ValorNaoPositivoException : Exception
+    {
+        private const string Mensagem = "ERRO - A propriedade '{0}' não pode possuir valor negativo. Valor informado: {1}";
+
+        public ValorNaoPositivoException(string argumento, double valor) : base(string.Format(Mensagem, argumento, valor))
+        {
+
+        }
+    }
+}
