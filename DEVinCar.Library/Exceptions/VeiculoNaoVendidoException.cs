8527fb8c05483e196b5fe4f99d01242048d776e0
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-07-24 14:15:13 -0300

    add: VeiculoNaoVendidoException

---
+++ DEVinCar.Library/Exceptions/VeiculoNaoVendidoException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEVinCar.Library
+{
+    public class VeiculoNaoVendidoException : Exception
+    {
+        private const string Mensagem = "ERRO - Veículo {0} de chassi número {1} não foi vendido";
+
+        public VeiculoNaoVendidoException(string nome, int chassi) : base(string.Format(Mensagem, nome, chassi))
+        {
+            
+        }
+    }
+}
